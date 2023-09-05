0df7adcdce9fd4b176dffe9dd906bd1f1a92ed0f
tommyokoyo <okoyotommy@gmail.com>
2023-09-05 17:17:45 +0300

    First API - correction

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
