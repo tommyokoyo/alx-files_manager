64dcf9533ab3163f51556495c90b40b93d53cb71
tommyokoyo <okoyotommy@gmail.com>
2023-09-05 16:36:22 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(DB_URL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const userCollection = this.client.db().collection('users');
+    return userCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const filesCollection = this.client.db().collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
